572e407264c05b660005fb0e4a66aad9a9f35d59
xue414 <ey_xuesiyi@163.com>
2025-10-21 09:42:46 +0800

    first commit: add basic echo.py

--- echo.py
+++ echo.py
@@ -4,3 +4,11 @@ def echo():
 
 if __name__ == "__main__":
     echo()
+import sys 
+def echo(): 
+    shout = "-s" in sys.argv 
+    message = input("Enter something: ") 
+    print(message.upper() if shout else message) 
+if __name__ == "__main__": 
+    echo()
+    

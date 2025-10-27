3ac2a1d3b6dcbb7043e9f12eb278e5733b59426f
xue414 <ey_xuesiyi@163.com>
2025-10-27 20:17:27 +0800

    format code with black to pass CI check

--- echo.py
+++ echo.py
@@ -2,13 +2,17 @@ def echo():
     message = input("Enter something: ")
     print(message)
 
+
 if __name__ == "__main__":
     echo()
-import sys 
-def echo(): 
-    shout = "-s" in sys.argv 
-    message = input("Enter something: ") 
-    print(message.upper() if shout else message) 
-if __name__ == "__main__": 
+import sys
+
+
+def echo():
+    shout = "-s" in sys.argv
+    message = input("Enter something: ")
+    print(message.upper() if shout else message)
+
+
+if __name__ == "__main__":
     echo()
-    
